8d6a1b62b4cbf41e7a11a0f0e984f62f4a37bef2
Aishwarya5445 <aishkudirella@gmail.com>
2025-12-11 19:18:01 +0530

    Add files via upload

---
+++ week-1/math.py
@@ -0,0 +1,9 @@
+import math
+print("square root of 4 is ",math.sqrt(64))
+print("sin value of 0 degree:",math.sin(0))
+print("sin value of 90 degree:",math.sin(90))
+print("cos value of 0 degree:",math.cos(0))
+print("cos value of 90 degree:",math.cos(90))
+print("tan value of 45 degree:",math.tan(0))
+print("calculating pi:",math.pi)
+print("dividing pi by 2:",math.pi/2)
