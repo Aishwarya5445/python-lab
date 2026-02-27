b744c97d53fbb3ba0a4dbce2d7b94f3e91fa87e9
sai_kushal <saikushal.kolli@gmail.com>
2026-02-27 13:39:35 +0530

    updated week 9 programs

---
+++ week9/write_files.py
@@ -0,0 +1,14 @@
+import random
+
+file = open("random_numbers.txt", "w")
+
+for i in range(20):
+    num = random.randint(1, 100)
+    file.write(str(num) + "\n")
+
+file.close()
+print("20 random numbers written to random_numbers.txt")
+# reading the 20 random numebers from random_numbers.txt file
+file=open("random_numbers.txt","r")
+print("reading the 20 random numebers from random_numbers.txt file")
+print(file.read())
